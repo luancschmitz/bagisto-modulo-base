ac556fba98350855f99ad32aeaa68fd77327d1b7
Luan Carlos <luan_c_schmitz@outlook.com>
2020-04-21 23:06:56 -0300

    module base Bagisto

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# bagisto-modulo-base
+# Módulo Base Bagisto
+
+nesse repositório contém um exemplo de um módulo Base para o Bagisto.
+
+Bagisto é um framework para e-commerce baseado no Laravel.
+
+https://bagisto.com.br/
+
---
+++ VendorName/PackageName/src/Providers/PackageNameServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+namespace VendorName\PackageName\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class PackageNameServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap Services
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register Services
+     */
+    public function register()
+    {
+
+    }
+}
